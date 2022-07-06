67acd3dd3da8bb81cbf4a061213e73eb8573bcef
Timothy Spengler <spengler@quellwerke.de>
2022-07-06 13:13:45 +0200

    Initial commit: Folder Structure and Placeholder Files

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Wordle</title>
+
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="script" href="js/main.js">
+
+</head>
+<body>
+
+<h1>Hello World</h1>
+<img src="https://i.gifer.com/g0R9.gif" alt="loading circle">
+<h3>Waiting for some content...</h3>
+
+</body>
+</html>
